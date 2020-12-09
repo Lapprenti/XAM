219c5f43d507c36edc7d4651ca7812f045fc1fe4
Arnaud Lapprand <arnaud.lapprand@diiage.org>
2020-12-09 17:51:15 +0100

    Add project files.

---
+++ XAM/XAM/ViewModels/Page1ViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+
+namespace XAM.ViewModels
+{
+    public class Page1ViewModel : ViewModelBase, INavigationAware
+    {
+        private string _welcomeMsg;
+        public string WelcomeMsg
+        {
+            get { return _welcomeMsg; }
+            set { SetProperty(ref _welcomeMsg, value); }
+        }
+        public Page1ViewModel(INavigationService navigationService): base(navigationService)
+        {
+            Title = "Page 1";
+        }
+
+        public override void OnNavigatedTo(INavigationParameters parameters)
+        {
+            var nom = parameters.GetValue<String>("nom");
+            WelcomeMsg = $"Bienvenue {nom} !";
+        }
+    }
+}
