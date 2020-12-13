8d7b3b0f3bd80fd013d99a58d30c06af16273fa1
Arnaud Lapprand <arnaud.lapprand@diiage.org>
2020-12-13 23:17:19 +0100

    Corrected comments

--- Business/services/ErgastAPI.cs
+++ Business/services/ErgastAPI.cs
@@ -31,13 +31,13 @@ namespace Business.services
             // Creating refit instance
             var s = RestService.For<IErgastAPI>("http://ergast.com/api/f1");
 
-            // Call route that return all seasons and awaiting response
+            // Call route that return all drivers and awaiting response
             JObject response = await s.FindDriversForSeason(year);
 
             // Path to all drivers
             JToken token = response["MRData"]["DriverTable"]["Drivers"];
 
-            // Cast to List of seasons
+            // Cast to List of drivers
             var allDrivers = token.ToObject<List<Driver>>();
 
             return allDrivers;
@@ -48,13 +48,13 @@ namespace Business.services
             // Creating refit instance
             var s = RestService.For<IErgastAPI>("http://ergast.com/api/f1");
 
-            // Call route that return all seasons and awaiting response
+            // Call route that return all constructors and awaiting response
             JObject response = await s.FindConstructorsForSeason(year);
 
-            // Path to all drivers
+            // Path to all constructors
             JToken token = response["MRData"]["ConstructorTable"]["Constructors"];
 
-            // Cast to List of seasons
+            // Cast to List of constructors
             var allConstructors = token.ToObject<List<Constructor>>();
 
             return allConstructors;
